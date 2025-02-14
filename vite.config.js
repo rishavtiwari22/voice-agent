afb19ed13756d17787a81f43c3542eb2558cc308
rishavtiwari22 <tiwaririshav463@gmail.com>
2025-02-14 19:09:13 +0530

    latest commit

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@google/generative-ai': '/node_modules/@google/generative-ai',
-    },
+  optimizeDeps: {
+    include: ['@google/generative-ai'], 
   },
-  build: {
-    rollupOptions: {
-      external: ['@google/generative-ai'],
-    },
-  },
-
 });
